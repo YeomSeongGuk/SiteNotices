aea6e5d4f22de856c04e5b8b8b686ddaf4718e8e
YeomSeongGuk <newysk@gmail.com>
2013-03-25 01:26:06 +0900

    다수의 dom 선택후 알림방식 변경 및 팝업선택 기능 추가

--- jquery-sitenotices.js
+++ jquery-sitenotices.js
@@ -1,5 +1,39 @@
 (function($){
+	var maxSeq = 0;
+
+	var setCookie = function(cKey, cValue , expireDay){
+		var date = new Date(); 
+	    date.setDate(date.getDate() + expireDay);
+	    document.cookie = cKey + '=' + escape(cValue) + ';expires=' + date.toGMTString();
+	}
+	 
+	var delCookie = function(cKey){
+		var date = new Date();  
+	    var validity = -1;
+	    date.setDate(date.getDate() + validity);
+	    document.cookie = cKey + "=;expires=" + date.toGMTString();
+	}
+	 
+	var getCookie = function(cKey) {
+		var allcookies = document.cookie;
+		var cookies = allcookies.split("; ");
+		for (var i = 0; i < cookies.length; i++) {
+			var keyValues = cookies[i].split("=");
+		    if (keyValues[0] == cKey) {
+		    	return unescape(keyValues[1]);
+		    }
+		}
+		return "";
+	}
+
+	$.notice = {
+		resetCookie : function(key){
+			delCookie(key);
+		}
+	};
+
 	$.fn.notice = function(key, option){
+		
 		var defaults = {
 			width : 800 
 			,offsetLeft : 150
@@ -11,74 +45,53 @@
 		};
 		$.extend(defaults,option);
 		
-		var setCookie = function(cKey, cValue , expireDay){
-			var date = new Date(); 
-		    date.setDate(date.getDate() + expireDay);
-		    document.cookie = cKey + '=' + escape(cValue) + ';expires=' + date.toGMTString();
-		}
-		 
-		var delCookie = function(cKey){
-			var date = new Date();  
-		    var validity = -1;
-		    date.setDate(date.getDate() + validity);
-		    document.cookie = cKey + "=;expires=" + date.toGMTString();
-		}
-		 
-		var getCookie = function(cKey) {
-			var allcookies = document.cookie;
-			var cookies = allcookies.split("; ");
-			for (var i = 0; i < cookies.length; i++) {
-				var keyValues = cookies[i].split("=");
-			    if (keyValues[0] == cKey) {
-			    	return unescape(keyValues[1]);
-			    }
-			}
-			return "";
+		if(defaults.zIndex > maxSeq){
+			maxSeq = defaults.zIndex + 1;
 		}
 
-		var message = ""
-		 
-		this.each(function(){
-			message += $(this).html();
-		});
-		
-		var template = '';
-		template='<div class="siteNotices" style="">';
-		template+='<div class="siteNoticesContents">'+message+'</div>';
-		template+='<div class="siteNoticesCloseToday">';
-		template+='<label><input class="alarmCheck" type="checkbox" /><em>오늘 하루 이 창을 열지 않음</em></label>';
-		template+='<span class="alarmClose" ><em>X</em>닫기</span>';
-		template+='</div></div>';
-		 
-			 
 		var value = getCookie(key);
 		if(value){return;}
-		var target = $(template).appendTo(document.body)
-								 .offset({ top: defaults.offsetTop, left: defaults.offsetLeft })
-			 					 .width(defaults.width)
-			 					 .css("z-index" , defaults.zIndex).hide().fadeIn(defaults.fadeIn);
-			 
-		$(target).find(".alarmClose").click(function(){
-			target.fadeOut(defaults.fadeOut , function(){
-				$(this).remove();
-			});; 
-		});
-			 
-		$(target).find(".alarmCheck").click(function(){
-			setCookie(key,"done", 1);
-			target.fadeOut(defaults.fadeOut , function(){
-				$(this).remove();
-			});; 
-		});
+
+		return this.each(function(){
+
+			var message = $(this).html();
 		
-		return this;
-    }
-	
-	$.fn.resetCookie = function(key){
-		var date = new Date();  
-	    var validity = -1;
-	    date.setDate(date.getDate() + validity);
-	    document.cookie = key + "=;expires=" + date.toGMTString();
+			var template = '';
+			template='<div class="siteNotices" style="">';
+			template+='<div class="siteNoticesContents">'+message+'</div>';
+			template+='<div class="siteNoticesCloseToday">';
+			template+='<label><input class="alarmCheck" type="checkbox" /><em>오늘 하루 이 창을 열지 않음</em></label>';
+			template+='<span class="alarmClose" ><em>X</em>닫기</span>';
+			template+='</div></div>';
+
+			var target = $(template).appendTo(document.body)
+									 .offset({ top: defaults.offsetTop, left: defaults.offsetLeft })
+				 					 .width(defaults.width)
+				 					 .css("z-index" , defaults.zIndex).hide().fadeIn(defaults.fadeIn);
+				 
+			$(target).find(".alarmClose").click(function(){
+				target.fadeOut(defaults.fadeOut , function(){
+					$(this).remove();
+				});; 
+			});
+				 
+			$(target).find(".alarmCheck").click(function(){
+				setCookie(key,"done", 1);
+				target.fadeOut(defaults.fadeOut , function(){
+					$(this).remove();
+				});; 
+			});
+
+			//팝업 선택시 최상위로 올림
+			$(target).click(function(){
+				$(this).css("z-index" , maxSeq++);
+			});
+
+			//대상 Element 에게 현재 옵션을 저장
+			$.data(target[0] , "option" , defaults);
+
+		});	
+		
 		return this;
     }
 	
